3b9c2b6fb09fd1566459ecea0cd60e1c32822eb5
Dor Levi <dor_l@dwalla.co.il>
2019-12-09 10:46:35 +0200

    add jenkins file

---
+++ test.py
@@ -0,0 +1,16 @@
+#for i in range(1,11):
+#   print(i) 
+
+
+
+#Mylist = ["Dor","Linoy","Ronit", "Amnon"]
+#for name in Mylist:
+#    print ("My name is:" + name) 
+
+
+name = input("Enter your name : ")
+
+if name == "dor":
+    print ("Welcome to Web server" )
+else:
+ print ("accsess denied!")
