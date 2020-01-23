e107643b0f416bb34dbc67d5b38b5fe249455c72
dor105 <dorlevi708@gmail.com>
2020-01-23 08:05:40 -0500

    .

--- Hello.sh
+++ Hello.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 echo -n "Enter the username: "
@@ -6,5 +7,25 @@ read username
 printf "Enter the password:\n"
 read -s password
 
+for i in {1}
+do
+last="$(lastlog -u UserName)" #Username that the system knows
+done
 
-echo "hello $username " "Today is $(date)"
+echo "Welcome $username now is $(date)"
+echo "your Last login was $last"
+
+
+: '
+#!/bin/bash
+
+counter=1
+while [ $counter -le 10 ]
+do
+echo $counter
+((counter++))
+done
+echo All done
+'
+
+
