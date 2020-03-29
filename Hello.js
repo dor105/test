36dc5bfdb43261e52a75086ad5ee784b808dbff1
dor105 <dorlevi708@gmail.com>
2020-03-29 18:25:29 +0300

    Js project

---
+++ Hello.js
@@ -0,0 +1,10 @@
+var start = new Date().getTime();
+
+document.getElementById("shape").onclick = function () {
+
+    document.getElementById("shape").style.display = "none";
+    var end = new Date().getTime();
+    var timeTake = (end - start) / 1000;
+    document.getElementById("timetake").innerHTML = timeTake + "sec";
+    
+}
