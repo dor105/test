c76d1973ba099a6c55aa60d758b6025131797855
dor105 <dorlevi708@gmail.com>
2020-04-04 20:09:44 +0300

    .

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,1 +1,9 @@
-Get-Command
+Function Get-ComputerInformation
+{
+    ## Computer Sys
+    Get-WmiObject -Class Win32_ComputerSystem
+    ## Operating Sys
+    Get-WmiObject -class win32_OperatingSystem
+    ## Bios
+    Get-WmiObject -class win32_BIOS
+}
