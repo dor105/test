49da665103db169fba5e1fa0a71b255fbad46523
dor105 <dorlevi708@gmail.com>
2020-08-14 08:56:45 -0400

    sh

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#set -x
+
+# What to backup? 
+backup_files="Test.{htm,json,py}"
+
+# Backup to
+
+#dest="/backupfolder"
+
+# Create archive filename
+archive_file="/backupfolder/bck_`date +%Y%m%d`.tar"
+
+
+tar -cpzf /backupfolder/bck_`date +%Y%m%d`.tar Test.{htm,json,py}
+
+if [ `echo $?` -ne 0 ]
+then
+    echo "Backup Failed!"
+else
+    echo "Backup Success!"
+fi
